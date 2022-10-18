1d83bbd95a7e7597bbf5874130ae982a645d6f4f
xmosphere <58228974+xmosphere@users.noreply.github.com>
2022-10-18 13:18:49 -0400

    Update check_url.sh

--- bash/check_url.sh
+++ bash/check_url.sh
@@ -9,7 +9,7 @@
 	2nd parameter: api key for abuseidpb.com
 COMMENTS
 
-ip=$(nmap -n -sn developer.mozilla.org -oG - | awk '/Up$/{print $2}')
+ip=$(nmap -n -sn $1 -oG - | awk '/Up$/{print $2}')
 result=$(curl -G https://api.abuseipdb.com/api/v2/check \
   --data-urlencode "ipAddress="$ip \
   -d maxAgeInDays=90 \
